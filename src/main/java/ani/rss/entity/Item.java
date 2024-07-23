7e9f83d96c00c09fbd553ac18faa6dd8a99c248b
wushuo <2725730722@qq.com>
2024-07-23 22:24:33 +0800

    完善设置

--- src/main/java/ani/rss/entity/Item.java
+++ src/main/java/ani/rss/entity/Item.java
@@ -8,13 +8,24 @@ import java.io.Serializable;
 @Data
 @Accessors(chain = true)
 public class Item implements Serializable {
+    /**
+     * 标题
+     */
     private String title;
-
+    /**
+     * 重命名
+     */
     private String reName;
-
+    /**
+     * 种子
+     */
     private String torrent;
-
+    /**
+     * 大小
+     */
     private Integer length;
-
+    /**
+     * 集数
+     */
     private Integer episode;
 }
