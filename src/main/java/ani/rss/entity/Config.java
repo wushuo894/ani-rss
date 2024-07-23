9099a9d8f3ade80e00bbc2d209fde9859f763147
wushuo <2725730722@qq.com>
2024-07-23 09:03:53 +0800

    增加设置

---
+++ src/main/java/ani/rss/entity/Config.java
@@ -0,0 +1,16 @@
+package ani.rss.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@Accessors(chain = true)
+public class Config implements Serializable {
+    private String host;
+    private String username;
+    private String password;
+    private Boolean rename;
+    private Integer sleep;
+}
--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,4 +1,26 @@
 <template>
+  <el-dialog v-model="configDialogVisible" title="设置" center>
+    <el-form style="max-width: 600px" label-width="auto">
+      <el-form-item label="qBittorrent 地址">
+        <el-input v-model:model-value="config.host"></el-input>
+      </el-form-item>
+      <el-form-item label="用户名">
+        <el-input v-model:model-value="config.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model:model-value="config.password"></el-input>
+      </el-form-item>
+      <el-form-item label="间隔">
+        <el-input-number v-model:model-value="config.sleep"></el-input-number>
+      </el-form-item>
+      <el-form-item label="重命名">
+        <el-switch v-model:model-value="config.rename"></el-switch>
+      </el-form-item>
+      <div style="display: flex;justify-content: end;width: 100%;margin-top: 10px;">
+        <el-button :loading="configButtonLoading" @click="editConfig">确定</el-button>
+      </div>
+    </el-form>
+  </el-dialog>
   <el-dialog v-model="addDialogVisible" title="添加订阅" center>
     <el-form style="max-width: 600px" label-width="auto">
       <el-form-item label="RSS 地址">
@@ -119,6 +141,7 @@
           excludeValue = ''
       }">添加
       </el-button>
+      <el-button @click="showConfig">设置</el-button>
     </div>
   </div>
   <div style="margin: 0 10px">
@@ -147,7 +170,8 @@
             text-transform: none !important;">{{ item.url }}
             </div>
           </div>
-          <div style="display: flex;align-items: flex-end;justify-content:flex-end; flex-direction: column;position: absolute;right: 0;bottom: 0;">
+          <div
+              style="display: flex;align-items: flex-end;justify-content:flex-end; flex-direction: column;position: absolute;right: 0;bottom: 0;">
             <el-button @click="()=>{
             editDialogVisible = true
             ani = JSON.parse(JSON.stringify(item))
@@ -173,6 +197,18 @@ import {ref} from "vue";
 import {ElMessage} from 'element-plus'
 
 const title = ref('')
+
+const configDialogVisible = ref(false)
+const configButtonLoading = ref(false)
+
+const config = ref({
+  'rename': true,
+  'host': '',
+  'username': '',
+  'password': '',
+  'sleep': 5
+})
+
 
 const addDialogVisible = ref(false)
 const editDialogVisible = ref(false)
@@ -285,6 +321,39 @@ const delAni = (ani) => {
         getList()
       })
 }
+
+const showConfig = () => {
+  fetch('/api/config', {
+    'method': 'GET'
+  })
+      .then(res => res.json())
+      .then(res => {
+        if (res.code !== 200) {
+          ElMessage.error(res.message)
+          return
+        }
+        config.value = res.data
+        configDialogVisible.value = true
+      })
+}
+const editConfig = () => {
+  configButtonLoading.value = true
+  fetch('/api/config', {
+    'method': 'POST',
+    'body': JSON.stringify(config.value)
+  })
+      .then(res => res.json())
+      .then(res => {
+        configButtonLoading.value = false
+        if (res.code !== 200) {
+          ElMessage.error(res.message)
+          return
+        }
+        ElMessage.success(res.message)
+        configDialogVisible.value = false
+      })
+}
+
 
 const list = ref([])
 
