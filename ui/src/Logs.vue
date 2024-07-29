06950b821f1165e82956c06f8c6d8e3992672a44
wushuo <wushuo894@outlook.com>
2024-07-29 11:57:49 +0800

    支持查看支持时筛选日志级别

--- ui/src/Logs.vue
+++ ui/src/Logs.vue
@@ -1,10 +1,19 @@
 <template>
   <el-dialog v-model="logDialogVisible" title="日志" center>
+    <div>
+      <el-select v-model="selectValue" @change="()=>{getHtmlLogs()}">
+        <el-option v-for="item in options"
+                   :key="item"
+                   :label="item"
+                   :value="item"/>
+      </el-select>
+    </div>
     <div id="#logs" style="background-color:#2e3440ff;
-                  color:#d8dee9ff;"
+                  color:#d8dee9ff;
+                  margin-top: 5px;"
          v-loading="loading">
       <el-scrollbar ref="scrollbarRef" height="400px">
-        <div ref="innerRef" style="padding: 5px;" v-html="logs">
+        <div ref="innerRef" style="padding: 5px;" v-html="htmlLogs">
         </div>
       </el-scrollbar>
     </div>
@@ -18,10 +27,29 @@ import {codeToHtml} from 'shiki'
 
 const logDialogVisible = ref(false)
 const loading = ref(true)
-const logs = ref('')
+const logs = ref([])
 const scrollbarRef = ref()
 const innerRef = ref()
 const maxHeight = ref(0)
+
+const options = ['All', 'DEBUG', 'INFO', 'WARNING', 'ERROR']
+
+const selectValue = ref(options[0])
+const htmlLogs = ref('')
+
+const getHtmlLogs = async () => {
+  let log = logs.value
+  if (selectValue.value !== options[0]) {
+    log = log.filter(it => it.toString().includes(selectValue.value))
+  }
+  htmlLogs.value = await codeToHtml(log.join('\r\n'), {
+    lang: 'log',
+    theme: 'nord'
+  })
+  setTimeout(() => {
+    scrollbarRef.value?.setScrollTop(innerRef.value.clientHeight)
+  })
+}
 
 const showLogs = () => {
   logDialogVisible.value = true
@@ -34,15 +62,9 @@ const showLogs = () => {
           ElMessage.error(res.message)
           return
         }
-        logs.value = await codeToHtml(res.data.join('\r\n'), {
-          lang: 'log',
-          theme: 'nord'
-        })
-        setTimeout(() => {
-          scrollbarRef.value?.setScrollTop(innerRef.value.clientHeight)
-        })
+        logs.value = res.data
+        getHtmlLogs()
       })
-
 }
 
 defineExpose({showLogs})
