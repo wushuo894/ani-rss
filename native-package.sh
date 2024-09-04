3cee565dbd249b3c3b9ae5e1f5dc47a41ad229ee
wushuo <2725730722@qq.com>
2024-09-05 07:27:08 +0800

    增加native编译

--- native-package.sh
+++ native-package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /bin/bash ./package.sh
 cd target
-native-image -cp ./ani-rss-jar-with-dependencies.jar ani.rss.Main -o ani-rss -O2 -H:-CheckToolchain -H:+StaticExecutableWithDynamicLibC -H:+ReportExceptionStackTraces
+native-image -cp ./ani-rss-jar-with-dependencies.jar ani.rss.Main -o ani-rss -O2 -H:-CheckToolchain -H:+StaticExecutableWithDynamicLibC -H:+ReportExceptionStackTraces -march=compatibility
 cd ..
