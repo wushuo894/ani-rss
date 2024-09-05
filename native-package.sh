ba7699fd19c7470a234b0835e5fcecc6817c1095
wushuo <2725730722@qq.com>
2024-09-05 08:56:54 +0800

    增加native编译

--- native-package.sh
+++ native-package.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
+
+# 创建一个工具存储目录
+mkdir /usr/local/graal-aot-tools
+# 进入到这个目录
+cd /usr/local/graal-aot-tools
+# 下载 x86_64-linux-musl 工具链
+wget http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz
+# 解压下载到的 musl 工具链包
+tar -zxvf x86_64-linux-musl-native.tgz
+# 复制文件到工作根目录
+cp -r x86_64-linux-musl-native/* .
+# 下载 zlib 依赖
+wget https://zlib.net/current/zlib.tar.gz
+# 解压 zlib 依赖源码包
+tar -zxvf zlib.tar.gz
+
+export TOOLCHAIN_DIR="/usr/local/graal-aot-tools"
+export PATH="$TOOLCHAIN_DIR/bin:$PATH"
+export CC="$TOOLCHAIN_DIR/bin/gcc"
+
+./configure --prefix=$TOOLCHAIN_DIR --static
+make
+make install
+
+cd /home/runner/work/ani-rss/ani-rss
+
 /bin/bash ./package.sh
-cd target
-native-image -march=x86-64 -cp ./ani-rss-jar-with-dependencies.jar ani.rss.Main -o ani-rss -O2 -H:-CheckToolchain -H:+StaticExecutableWithDynamicLibC -H:+ReportExceptionStackTraces
+cd /home/runner/work/ani-rss/ani-rss/target
+native-image --gc=G1 --static --libc=musl --no-fallback -march=compatibility -cp ./ani-rss-jar-with-dependencies.jar ani.rss.Main -o ani-rss -O2 -H:-CheckToolchain -H:+StaticExecutableWithDynamicLibC -H:+ReportExceptionStackTraces
 cd ..
