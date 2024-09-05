3174161163165d0b25e3acfafeca30ace1f053ef
wushuo <2725730722@qq.com>
2024-09-05 10:55:21 +0800

    增加native编译

--- native-package.sh
+++ native-package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-/bin/bash ./package.sh
-cd /home/runner/work/ani-rss/ani-rss/target
+wget https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-17.0.9/graalvm-community-jdk-17.0.9_linux-x64_bin.tar.gz
+
+tar -xvf graalvm-community-jdk-17.0.9_linux-x64_bin.tar.gz
+export PATH="$PATH:/home/runner/work/ani-rss/ani-rss/graalvm-community-openjdk-17.0.9+9.1/bin"
+
 native-image -H:+StaticExecutableWithDynamicLibC -march=compatibility -cp ./ani-rss-jar-with-dependencies.jar ani.rss.Main -o ani-rss
-cd ..
