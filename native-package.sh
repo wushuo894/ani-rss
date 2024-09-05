68128a8ecf06aefa28a7ba0fb13346c2218efe26
wushuo <2725730722@qq.com>
2024-09-05 10:02:05 +0800

    增加native编译

--- native-package.sh
+++ native-package.sh
@@ -30,6 +30,6 @@ cd /home/runner/work/ani-rss/ani-rss
 
 /bin/bash ./package.sh
 cd /home/runner/work/ani-rss/ani-rss/target
-native-image --static --libc=musl march=compatibility -cp ./ani-rss-jar-with-dependencies.jar ani.rss.Main -o ani-rss-x86_64-linux-musl -H:+StaticExecutableWithDynamicLibC -H:+ReportExceptionStackTraces
+native-image --static --libc=musl -march=compatibility -cp ./ani-rss-jar-with-dependencies.jar ani.rss.Main -o ani-rss-x86_64-linux-musl -H:+StaticExecutableWithDynamicLibC -H:+ReportExceptionStackTraces
 upx ani-rss-x86_64-linux-musl
 cd ..
