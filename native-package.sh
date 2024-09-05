ddb869da8b7e40033a8d7bd823d64216aa7ed9d2
wushuo <2725730722@qq.com>
2024-09-05 11:00:28 +0800

    增加native编译

--- native-package.sh
+++ native-package.sh
@@ -4,4 +4,4 @@ wget https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-17.0.9/g
 tar -xvf graalvm-community-jdk-17.0.9_linux-x64_bin.tar.gz
 export PATH="$PATH:/home/runner/work/ani-rss/ani-rss/graalvm-community-openjdk-17.0.9+9.1/bin"
 
-native-image -H:+StaticExecutableWithDynamicLibC -march=compatibility -cp ./ani-rss-jar-with-dependencies.jar ani.rss.Main -o ani-rss
+native-image -H:+StaticExecutableWithDynamicLibC -march=compatibility -cp ./ani-rss-jar-with-dependencies.jar ani.rss.Main -o /target/ani-rss
