c72dacf082fa292be86c170df2b73a89aab08cf8
wushuo <wushuo894@outlook.com>
2024-09-07 15:19:18 +0800

    添加图片缓存 修复无法获取图片问题

--- src/main/java/ani/rss/action/FileAction.java
+++ src/main/java/ani/rss/action/FileAction.java
@@ -3,19 +3,24 @@ package ani.rss.action;
 import ani.rss.annotation.Auth;
 import ani.rss.annotation.Path;
 import ani.rss.util.ConfigUtil;
+import ani.rss.util.HttpReq;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.URLUtil;
 import cn.hutool.http.ContentType;
+import cn.hutool.http.HttpConnection;
 import cn.hutool.http.server.HttpServerRequest;
 import cn.hutool.http.server.HttpServerResponse;
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.function.Consumer;
 
 @Slf4j
 @Auth(value = false)
@@ -23,26 +28,83 @@ import java.io.OutputStream;
 public class FileAction implements BaseAction {
 
     @Override
-    public void doAction(HttpServerRequest req, HttpServerResponse res) throws IOException {
-        String filename = req.getParam("filename");
+    public void doAction(HttpServerRequest request, HttpServerResponse response) throws IOException {
+        String img = request.getParam("img");
+        if (StrUtil.isNotBlank(img)) {
+            img = new String(Base64.getDecoder().decode(img), StandardCharsets.UTF_8);
+            response.setContentType(FileUtil.getMimeType(URLUtil.getPath(img)));
+
+            File configDir = ConfigUtil.getConfigDir();
+
+            File file = new File(URLUtil.getPath(img));
+            configDir = new File(configDir + "/img/" + file.getParentFile().getName());
+            FileUtil.mkdir(configDir);
+
+            File imgFile = new File(configDir, file.getName());
+            if (imgFile.exists()) {
+                @Cleanup
+                BufferedInputStream inputStream = FileUtil.getInputStream(imgFile);
+                @Cleanup
+                OutputStream out = response.getOut();
+                IoUtil.copy(inputStream, out);
+                return;
+            }
+
+            getImg(img, is -> {
+                try {
+                    FileUtil.writeFromStream(is, imgFile, true);
+                    @Cleanup
+                    BufferedInputStream inputStream = FileUtil.getInputStream(imgFile);
+                    @Cleanup
+                    OutputStream out = response.getOut();
+                    IoUtil.copy(inputStream, out);
+                } catch (Exception ignored) {
+                }
+            });
+            return;
+        }
+
+
+        String filename = request.getParam("filename");
         if (StrUtil.isBlank(filename)) {
-            res.sendOk();
+            response.send404("404");
             return;
         }
         String mimeType = FileUtil.getMimeType(filename);
         if (StrUtil.isBlank(mimeType)) {
-            res.setContentType(mimeType);
+            response.setContentType(mimeType);
         } else {
-            res.setContentType(ContentType.OCTET_STREAM.getValue());
+            response.setContentType(ContentType.OCTET_STREAM.getValue());
         }
 
-        res.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
 
         File configDir = ConfigUtil.getConfigDir();
         @Cleanup
         FileInputStream inputStream = IoUtil.toStream(new File(configDir + "/files/" + filename));
         @Cleanup
-        OutputStream out = res.getOut();
+        OutputStream out = response.getOut();
         IoUtil.copy(inputStream, out);
     }
+
+    public static void getImg(String url, Consumer<InputStream> consumer) {
+        URI host = URLUtil.getHost(URLUtil.url(url));
+        HttpReq.get(url)
+                .then(res -> {
+                    HttpConnection httpConnection = (HttpConnection) ReflectUtil.getFieldValue(res, "httpConnection");
+                    URI host1 = URLUtil.getHost(httpConnection.getUrl());
+                    if (host.toString().equals(host1.toString())) {
+                        try (InputStream inputStream = res.bodyStream()) {
+                            consumer.accept(inputStream);
+                        } catch (Exception e) {
+                            log.error(e.getMessage());
+                            log.debug(e.getMessage(), e);
+                        }
+                        return;
+                    }
+                    String newUrl = url.replace(host.toString(), host1.toString());
+                    getImg(newUrl, consumer);
+                });
+    }
+
 }
--- ui/src/App.vue
+++ ui/src/App.vue
@@ -28,7 +28,7 @@
       <div v-for="(item,index) in searchList().slice((currentPage-1)*pageSize,(currentPage-1)*pageSize+pageSize)">
         <el-card shadow="never">
           <div style="display: flex;width: 100%;">
-            <img :src="'/api/file?filename='+item['cover']" height="130" width="92" :alt="item.title">
+            <img :src="'/api/file?filename='+item['cover']" height="130" width="92" :alt="item.title" style="border-radius: 4px;">
             <div style="flex-grow: 1;position: relative;">
               <div style="margin-left: 10px;">
                 <div style="
