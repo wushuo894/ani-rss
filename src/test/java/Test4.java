dabb9bd1a03a729e1d203d9d272d3d3709d38a6e
wushuo <2725730722@qq.com>
2024-09-07 14:00:46 +0800

    修复 夜游生活 空指针问题

--- src/test/java/Test4.java
+++ src/test/java/Test4.java
@@ -1,10 +1,9 @@
+import ani.rss.util.AniUtil;
 import ani.rss.util.ConfigUtil;
-import ani.rss.util.MailUtils;
 
 public class Test4 {
     public static void main(String[] args) {
         ConfigUtil.load();
-        ConfigUtil.CONFIG.setMailAddressee("wushuo894@outlook.com");
-        MailUtils.send("test");
+        AniUtil.getAni("https://mikanani.me/RSS/Bangumi?bangumiId=3353&subgroupid=203");
     }
 }
