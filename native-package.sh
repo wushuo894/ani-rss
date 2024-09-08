b1976f42a60c420eeab5089be7938cfd86c682de
wushuo <2725730722@qq.com>
2024-09-08 21:41:47 +0800

    docs

--- native-package.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir target
-native-image -H:+StaticExecutableWithDynamicLibC -march=compatibility -cp ./ani-rss-jar-with-dependencies.jar ani.rss.Main -o target/ani-rss --no-fallback
