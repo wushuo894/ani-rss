367390ceef87368ca01372b3fb82a88afcb1cc4c
wushuo <2725730722@qq.com>
2024-09-09 06:27:37 +0800

    robots.txt

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+jar="ani-rss-jar-with-dependencies.jar"
+
+pid=$(pgrep -f "$jar")
+if [ -n "$pid" ]; then
+    echo "Stopping process $pid - $jar"
+    kill "$pid"
+fi
+
 while :
 do
-    java -jar -Xmx2g ani-rss-jar-with-dependencies.jar
+    java -jar -Xmx2g $jar
     if [ $? -ne 0 ]; then
         break
     fi
