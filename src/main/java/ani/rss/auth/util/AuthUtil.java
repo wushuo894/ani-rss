b2b9218445d15be3ab6838690539258165bf0c7a
wushuo <2725730722@qq.com>
2024-09-09 05:04:13 +0800

    优化更新逻辑

--- src/main/java/ani/rss/auth/util/AuthUtil.java
+++ src/main/java/ani/rss/auth/util/AuthUtil.java
@@ -11,20 +11,20 @@ import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.crypto.digest.MD5;
-import cn.hutool.crypto.symmetric.AES;
 import cn.hutool.http.server.HttpServerRequest;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
+import lombok.extern.slf4j.Slf4j;
 
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class AuthUtil {
-    private static AES AES;
+    private static String KEY;
     private static final MD5 MD5 = new MD5();
     private static final Gson GSON = new Gson();
     private static final Map<String, Function<HttpServerRequest, Boolean>> MAP = new HashMap<>();
@@ -37,12 +37,12 @@ public class AuthUtil {
      * 刷新密钥
      */
     public static synchronized void resetKey() {
-        String key = RandomUtil.randomString(32);
-        AES = new AES(key.getBytes(StandardCharsets.UTF_8));
+        KEY = RandomUtil.randomString(32);
     }
 
     public static synchronized String getAuth(Login login) {
-        return MD5.digestHex(AES.encryptBase64(GSON.toJson(login)));
+        login.setKey(KEY);
+        return MD5.digestHex(GSON.toJson(login));
     }
 
     public static Login getLogin() {
@@ -53,9 +53,14 @@ public class AuthUtil {
     }
 
     public static String getIp() {
-        HttpServerRequest request = ServerUtil.REQUEST.get();
-        HttpExchange httpExchange = (HttpExchange) ReflectUtil.getFieldValue(request, "httpExchange");
-        return httpExchange.getRemoteAddress().getAddress().getHostAddress();
+        try {
+            HttpServerRequest request = ServerUtil.REQUEST.get();
+            HttpExchange httpExchange = (HttpExchange) ReflectUtil.getFieldValue(request, "httpExchange");
+            return httpExchange.getRemoteAddress().getAddress().getHostAddress();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return "未知";
     }
 
     public static Boolean test(HttpServerRequest request, AuthType authType) {
