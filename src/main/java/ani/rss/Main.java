010e39f138ce91c01df34cd9c20ef03bbdfbae24
wushuo <2725730722@qq.com>
2024-09-09 22:16:53 +0800

    整理代码

--- src/main/java/ani/rss/Main.java
+++ src/main/java/ani/rss/Main.java
@@ -9,13 +9,14 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            MenuUtil.start(args);
             ConfigUtil.load();
             AniUtil.load();
             ThreadUtil.execute(() -> ServerUtil.create(args).start());
             TaskUtil.start();
             String version = MavenUtil.getVersion();
             log.info("version {}", version);
+
+            MenuUtil.start(args);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             System.exit(1);
