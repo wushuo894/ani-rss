7c2e7ada3998f949a3aa47d7c2ece943f4e1772b
wushuo <2725730722@qq.com>
2024-09-12 07:46:14 +0800

    windows打包

---
+++ package-win.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir ani-rss
+cp target/ani-rss-launcher.exe ani-rss/
+
+sudo apt update
+sudo apt install zip unzip
+wget https://github.com/ojdkbuild/ojdkbuild/releases/download/java-17-openjdk-17.0.3.0.6-1/java-17-openjdk-17.0.3.0.6-1.jre.win.x86_64.zip
+unzip java-17-openjdk-17.0.3.0.6-1.jre.win.x86_64.zip
+mv java-17-openjdk-17.0.3.0.6-1.jre.win.x86_64 ani-rss/jre
+zip -r ani-rss.win.x86_64.zip ani-rss
+
+ls ani-rss.win.x86_64.zip -al
