d54cc2e6ef6da07ada24c19a388a92d46d466d91
wushuo <2725730722@qq.com>
2024-09-15 00:49:32 +0800

    优化异常处理

--- src/main/java/ani/rss/util/HttpRequestPlus.java
+++ src/main/java/ani/rss/util/HttpRequestPlus.java
@@ -3,12 +3,36 @@ package ani.rss.util;
 import cn.hutool.core.net.url.UrlBuilder;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
+import cn.hutool.http.Method;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class HttpRequestPlus extends HttpRequest {
     public HttpRequestPlus(UrlBuilder url) {
         super(url);
+    }
+
+    public static HttpRequest of(UrlBuilder url) {
+        return new HttpRequestPlus(url);
+    }
+
+    public static HttpRequest of(String url) {
+        return HttpRequestPlus.of(UrlBuilder.ofHttp(url, StandardCharsets.UTF_8));
+    }
+
+    public static HttpRequest of(String url, Charset charset) {
+        return HttpRequestPlus.of(UrlBuilder.ofHttp(url, charset));
+    }
+
+    public static HttpRequest get(String url) {
+        return HttpRequestPlus.of(url).method(Method.GET);
+    }
+
+    public static HttpRequest post(String url) {
+        return HttpRequestPlus.of(url).method(Method.POST);
     }
 
     @Override
@@ -17,8 +41,7 @@ public class HttpRequestPlus extends HttpRequest {
         try {
             return super.execute(isAsync);
         } catch (Exception e) {
-            log.error("{} {}", url, e.getMessage());
-            log.error(e.getMessage(), e);
+            log.error("url ===> {} error===> {}", url, e.getMessage());
             throw new RuntimeException(e);
         }
     }
