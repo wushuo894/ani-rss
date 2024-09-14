d6001844434ae8fc258ad5388786e1efbca1e230
wushuo <2725730722@qq.com>
2024-09-14 23:20:03 +0800

    优化异常处理

--- src/main/java/ani/rss/util/MessageUtil.java
+++ src/main/java/ani/rss/util/MessageUtil.java
@@ -7,27 +7,32 @@ import ani.rss.msg.Message;
 import ani.rss.msg.Telegram;
 import ani.rss.msg.WebHook;
 import cn.hutool.core.thread.ThreadUtil;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MessageUtil {
     public static final Message mailMessage = new Mail();
     public static final Message telegramMessage = new Telegram();
     public static final Message webHookMessage = new WebHook();
 
     public static synchronized void send(Config config, Ani ani, String text) {
-        Boolean mail = config.getMail();
-        if (mail) {
-            ThreadUtil.execute(() -> mailMessage.send(config, ani, text));
-        }
+        try {
+            Boolean mail = config.getMail();
+            if (mail) {
+                ThreadUtil.execute(() -> mailMessage.send(config, ani, text));
+            }
 
-        Boolean telegram = config.getTelegram();
-        if (telegram) {
-            ThreadUtil.execute(() -> telegramMessage.send(config, ani, text));
-        }
+            Boolean telegram = config.getTelegram();
+            if (telegram) {
+                ThreadUtil.execute(() -> telegramMessage.send(config, ani, text));
+            }
 
-        Boolean webHook = config.getWebHook();
-        if (webHook) {
-            ThreadUtil.execute(() -> webHookMessage.send(config, ani, text));
+            Boolean webHook = config.getWebHook();
+            if (webHook) {
+                ThreadUtil.execute(() -> webHookMessage.send(config, ani, text));
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
-
     }
 }
