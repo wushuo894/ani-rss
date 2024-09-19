95f48113ecb80afa40de89fbc882219f759ba4bd
wushuo <2725730722@qq.com>
2024-09-19 19:46:31 +0800

    更新版本

--- ui/src/home/BackRss.vue
+++ ui/src/home/BackRss.vue
@@ -1,5 +1,8 @@
 <template>
-  <Mikan ref="mikan" @add="url => backRss[editIndex] = url"/>
+  <Mikan ref="mikan" @add="url => {
+    plus()
+    backRss[editIndex] = url
+  }"/>
   <el-dialog v-model="dialogVisible" title="备用订阅" center v-if="dialogVisible">
     <div style="display: flex;width: 100%;">
       <div>
@@ -7,10 +10,7 @@
       </div>
       <div style="margin: 3px;"></div>
       <div>
-        <el-button @click="()=>{
-        plus()
-        mikan?.show()
-      }" text bg icon="VideoCamera"/>
+        <el-button @click="mikan?.show" text bg icon="VideoCamera"/>
       </div>
     </div>
     <el-scrollbar>
