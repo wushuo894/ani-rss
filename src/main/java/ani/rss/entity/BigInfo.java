e46f5ea7e80d665a7325f90f3a7627e9be73cb1e
wushuo <2725730722@qq.com>
2024-09-24 11:22:33 +0800

    修改bgm信息的获取方式

---
+++ src/main/java/ani/rss/entity/BigInfo.java
@@ -0,0 +1,35 @@
+package ani.rss.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+
+@Data
+@Accessors(chain = true)
+public class BigInfo {
+    /**
+     * 名称
+     */
+    private String nameCn;
+
+    /**
+     * 评分
+     */
+    private double score;
+
+    /**
+     * 集数
+     */
+    private Integer eps;
+
+    /**
+     * ova
+     */
+    private Boolean ova;
+
+    /**
+     * 时间
+     */
+    private LocalDateTime date;
+}
