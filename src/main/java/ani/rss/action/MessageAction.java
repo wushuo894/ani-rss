f0638409f6b8b5cf445f3c1c0910d69fd15b6b87
wushuo <2725730722@qq.com>
2024-09-29 16:34:37 +0800

    测试通知时可以发送图片了
    优化了qq邮箱的通知

--- src/main/java/ani/rss/action/MessageAction.java
+++ src/main/java/ani/rss/action/MessageAction.java
@@ -2,8 +2,12 @@ package ani.rss.action;
 
 import ani.rss.annotation.Auth;
 import ani.rss.annotation.Path;
+import ani.rss.entity.Ani;
+import ani.rss.entity.BigInfo;
 import ani.rss.entity.Config;
 import ani.rss.msg.Message;
+import ani.rss.util.AniUtil;
+import ani.rss.util.BgmUtil;
 import cn.hutool.core.util.ClassUtil;
 import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.StrUtil;
@@ -25,7 +29,12 @@ public class MessageAction implements BaseAction {
         Config config = getBody(Config.class);
         Class<Object> loadClass = ClassUtil.loadClass("ani.rss.msg." + s);
         Message message = (Message) ReflectUtil.newInstance(loadClass);
-        Boolean test = message.send(config, null, "test");
+        Ani ani = new Ani();
+        ani.setBgmUrl("https://bgm.tv/subject/424883");
+        BigInfo bgmInfo = BgmUtil.getBgmInfo(ani);
+        String image = bgmInfo.getImage();
+        ani.setCover(AniUtil.saveJpg(image));
+        Boolean test = message.send(config, ani, "test");
         if (test) {
             resultSuccess();
             return;
