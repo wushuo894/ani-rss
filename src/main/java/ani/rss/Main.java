aba848b6e6463a90ace5b127e7561a376cc402e2
wushuo <2725730722@qq.com>
2024-10-01 22:58:34 +0800

    测试代理

--- src/main/java/ani/rss/Main.java
+++ src/main/java/ani/rss/Main.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 
     public static void main(String[] args) {
+        System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
         try {
             UpdateUtil.manageWindows();
             ConfigUtil.load();
