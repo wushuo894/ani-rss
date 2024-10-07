e6203b03fd61266ea55dce477028fb89e0839521
wushuo <2725730722@qq.com>
2024-10-07 13:03:05 +0800

    可在添加时检索 Bangumi 信息

--- ui/src/home/Add.vue
+++ ui/src/home/Add.vue
@@ -1,5 +1,9 @@
 <template>
   <Mikan ref="mikan" @add="args => ani.url = args.url"/>
+  <Bgm ref="bgmRef" @add="it => {
+    ani.title = it['name_cn']
+    ani.bgmUrl = it.url
+  }"/>
   <el-dialog v-model="dialogVisible" title="添加订阅" center v-if="dialogVisible">
     <div v-if="showRss" @keydown.enter="getRss">
       <el-tabs tab-position="left" v-model="activeName">
@@ -40,10 +44,16 @@
                 event.preventDefault()
              }">
             <el-form-item label="番剧名称">
-              <el-input
-                  v-model:model-value="ani.title"
-                  placeholder="可以为空 如果获取失败建议补全"
-              />
+              <div style="display: flex;width: 100%;">
+                <div style="flex: 1">
+                  <el-input
+                      v-model:model-value="ani.title"
+                      placeholder="可以为空 如果获取失败建议补全"
+                  />
+                </div>
+                <div style="width: 4px;"></div>
+                <el-button text bg icon="Search" @click="bgmRef?.show(ani.title)"/>
+              </div>
             </el-form-item>
             <el-form-item label="BgmUrl">
               <el-input
@@ -68,10 +78,16 @@
                 event.preventDefault()
              }">
             <el-form-item label="番剧名称">
-              <el-input
-                  v-model:model-value="ani.title"
-                  placeholder="可以为空 如果获取失败建议补全"
-              />
+              <div style="display: flex;width: 100%;">
+                <div style="flex: 1">
+                  <el-input
+                      v-model:model-value="ani.title"
+                      placeholder="可以为空 如果获取失败建议补全"
+                  />
+                </div>
+                <div style="width: 4px;"></div>
+                <el-button text bg icon="Search" @click="bgmRef?.show(ani.title)"/>
+              </div>
             </el-form-item>
             <el-form-item label="BgmUrl">
               <el-input
@@ -112,9 +128,11 @@ import {ElMessage} from "element-plus";
 import api from "../api.js";
 import Mikan from "./Mikan.vue";
 import Ani from "./Ani.vue";
+import Bgm from "./Bgm.vue";
 
 const showRss = ref(true)
 const mikan = ref()
+const bgmRef = ref()
 
 const dialogVisible = ref(false)
 
