3797e0f63682a1f51dbe8000e9a16c3a23fc7743
wushuo <wushuo894@outlook.com>
2024-10-14 23:39:10 +0800

    支持系统通知
    优化日志显示

--- src/test/java/Test6.java
+++ src/test/java/Test6.java
@@ -10,7 +10,7 @@ public class Test6 {
         Config config = ConfigUtil.CONFIG;
         Ani ani = AniUtil.getAni("https://mikanani.me/RSS/Bangumi?bangumiId=3394&subgroupid=583");
         Telegram telegram = new Telegram();
-        Boolean test = telegram.send(config, ani, "test");
+        Boolean test = telegram.send(config, ani, null, "test");
         System.out.println(test);
     }
 }
