546b6f399dd55fa792d2d6b4a796c021ad355b83
wushuo <2725730722@qq.com>
2024-10-15 17:57:20 +0800

    优化通知

--- src/main/java/ani/rss/util/MessageUtil.java
+++ src/main/java/ani/rss/util/MessageUtil.java
@@ -3,29 +3,23 @@ package ani.rss.util;
 import ani.rss.entity.Ani;
 import ani.rss.entity.Config;
 import ani.rss.enums.MessageEnum;
-import ani.rss.msg.*;
+import ani.rss.msg.Message;
+import cn.hutool.core.bean.DynaBean;
 import cn.hutool.core.thread.ExecutorBuilder;
+import cn.hutool.core.util.ClassUtil;
+import cn.hutool.core.util.ReflectUtil;
+import lombok.Synchronized;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 
 @Slf4j
 public class MessageUtil {
-    private static final ExecutorService EXECUTOR = ExecutorBuilder.create()
-            .setCorePoolSize(1)
-            .setMaxPoolSize(1)
-            .setWorkQueue(new LinkedBlockingQueue<>(128))
-            .build();
+    private static final Map<String, ExecutorService> SERVICE_MAP = new HashMap<>();
 
-    public static final Message mailMessage = new Mail();
-    public static final Message telegramMessage = new Telegram();
-    public static final Message webHookMessage = new WebHook();
-    public static final Message serverChanMessage = new ServerChan();
-    public static final Message systemMessage = new SystemMsg();
-
+    @Synchronized("SERVICE_MAP")
     public static synchronized void send(Config config, Ani ani, String text, MessageEnum messageEnum) {
         List<MessageEnum> messageList = config.getMessageList();
         if (Objects.nonNull(messageEnum)) {
@@ -33,33 +27,37 @@ public class MessageUtil {
                 return;
             }
         }
-        try {
-            Boolean mail = config.getMail();
-            if (mail) {
-                EXECUTOR.execute(() -> mailMessage.send(config, ani, messageEnum, text));
+        Set<Class<?>> classes = ClassUtil.scanPackage("ani.rss.msg");
+        DynaBean dynaBean = DynaBean.create(config);
+        for (Class<?> aClass : classes) {
+            if (aClass.isInterface()) {
+                continue;
+            }
+            String name = aClass.getSimpleName();
+            name = name.substring(0, 1).toLowerCase() + name.substring(1);
+            Object b = dynaBean.get(name);
+            if (Objects.isNull(b)) {
+                continue;
+            }
+            if (!(b instanceof Boolean)) {
+                continue;
+            }
+            if (!(Boolean) b) {
+                continue;
             }
 
-            Boolean telegram = config.getTelegram();
-            if (telegram) {
-                EXECUTOR.execute(() -> telegramMessage.send(config, ani, messageEnum, text));
+            ExecutorService executor = SERVICE_MAP.get(name);
+            if (Objects.isNull(executor)) {
+                executor = ExecutorBuilder.create()
+                        .setCorePoolSize(1)
+                        .setMaxPoolSize(1)
+                        .setWorkQueue(new LinkedBlockingQueue<>(64))
+                        .build();
+                SERVICE_MAP.put(name, executor);
             }
 
-            Boolean webHook = config.getWebHook();
-            if (webHook) {
-                EXECUTOR.execute(() -> webHookMessage.send(config, ani, messageEnum, text));
-            }
-
-            Boolean serverChan = config.getServerChan();
-            if (serverChan) {
-                EXECUTOR.execute(() -> serverChanMessage.send(config, ani, messageEnum, text));
-            }
-
-            Boolean systemMsg = config.getSystemMsg();
-            if (systemMsg) {
-                EXECUTOR.execute(() -> systemMessage.send(config, ani, messageEnum, text));
-            }
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
+            Message message = (Message) ReflectUtil.newInstance(aClass);
+            executor.execute(() -> message.send(config, ani, messageEnum, text));
         }
     }
 }
