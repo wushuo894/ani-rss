900f6e2898cdb525e0f18310a93530fc016525d0
wushuo <wushuo894@outlook.com>
2024-10-21 00:09:20 +0800

    优化年份剔除

--- src/main/java/ani/rss/enums/StringEnum.java
+++ src/main/java/ani/rss/enums/StringEnum.java
@@ -2,4 +2,6 @@ package ani.rss.enums;
 
 public class StringEnum {
     public static final String SEASON_REG = "[Ss](\\d+)[Ee](\\d+(\\.5)?)";
+    public static final String YEAR_REG = " ?\\((19|20)\\d{2}\\)";
+    public static final String TMDB_ID_REG = " ?\\[tmdbid=(\\d+)]";
 }
