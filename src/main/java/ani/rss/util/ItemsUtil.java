6239d8bbfb12a9c7d0f3ec06d13b6119baaec8ab
wushuo <wushuo894@outlook.com>
2024-11-01 23:57:12 +0800

    1.1.169

--- src/main/java/ani/rss/util/ItemsUtil.java
+++ src/main/java/ani/rss/util/ItemsUtil.java
@@ -188,7 +188,7 @@ public class ItemsUtil {
                     .setPubDate(pubDate);
 
             // 进行过滤
-            if (exclude.stream().anyMatch(s -> ReUtil.contains(s, addNewItem.getTitle()))) {
+            if (exclude.stream().allMatch(s -> ReUtil.contains(s, addNewItem.getTitle()))) {
                 continue;
             }
 
