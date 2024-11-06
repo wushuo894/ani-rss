0ca729bc8dac0c199ffa15866b9adee9cedf19c3
wushuo <wushuo894@outlook.com>
2024-11-06 21:46:45 +0800

    增加获取TMDB台湾地区标题开关

--- src/main/java/ani/rss/util/ConfigUtil.java
+++ src/main/java/ani/rss/util/ConfigUtil.java
@@ -90,6 +90,7 @@ public class ConfigUtil {
                 .setWebHook(false)
                 .setTmdb(false)
                 .setTmdbId(false)
+                .setTmdbTw(false)
                 .setIpWhitelist(false)
                 .setIpWhitelistStr("")
                 .setWebHookBody("")
