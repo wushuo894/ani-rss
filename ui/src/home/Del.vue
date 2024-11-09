8f47625fe64e0d01a1225d46dea8c4205753a7fe
wushuo <wushuo894@outlook.com>
2024-11-09 23:57:53 +0800

    增加删除时可选择删除已下载的文件选项

---
+++ ui/src/home/Del.vue
@@ -0,0 +1,72 @@
+<template>
+  <el-dialog v-model="dialogVisible" v-if="dialogVisible" center align-center width="300">
+    <div>
+      <div v-if="aniList.length === 1">
+        <el-text class="mx-1" size="large">是否删除 {{ aniList[0].title }} 第{{ aniList[0].season }}季?</el-text>
+      </div>
+      <div v-else>
+        <el-text class="mx-1" size="large">是否删除共 {{aniList.length}} 个订阅?</el-text>
+      </div>
+      <el-checkbox v-model="deleteFiles">同时删除本地已下载的文件</el-checkbox>
+    </div>
+    <div style="width:100%;display: flex;justify-content: end;margin-top: 8px;">
+      <el-button icon="Check" :loading="okLoading" @click="delAni" text bg type="danger">确定
+      </el-button>
+      <el-button icon="Close" bg text @click="dialogVisible = false">取消</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+import api from "../api.js";
+import {ElMessage} from "element-plus";
+
+const dialogVisible = ref(false)
+
+const aniList = ref([
+  {
+    'url': '',
+    'season': 1,
+    'offset': 0,
+    'title': '',
+    'themoviedbName': '',
+    'exclude': [],
+    'enable': true,
+    'ova': false,
+    'totalEpisodeNumber': '',
+    'customDownloadPath': false,
+    'downloadPath': ''
+  }
+])
+
+let okLoading = ref(false)
+let deleteFiles = ref(false)
+
+const delAni = () => {
+  okLoading.value = true
+  api.del('api/ani?deleteFiles=' + deleteFiles.value, aniList.value.map(it => it['id']))
+      .then(res => {
+        ElMessage.success(res.message)
+        emit('load')
+        dialogVisible.value = false
+      })
+      .finally(() => {
+        okLoading.value = false
+      });
+}
+
+const show = (anis) => {
+  aniList.value = JSON.parse(JSON.stringify(anis))
+  deleteFiles.value = false
+  dialogVisible.value = true
+}
+
+defineExpose({
+  show
+})
+
+const emit = defineEmits(['load'])
+</script>
+
