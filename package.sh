0f4fd7a80943d65761a83ddc7c91fa124e993102
wushuo <wushuo894@outlook.com>
2024-11-23 22:35:36 +0800

    更新版本

--- package.sh
+++ package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf ../src/main/resources/dist
 mkdir -p src/main/resources/dist
 cd ui
 npm install pnpm -g
@@ -7,6 +8,6 @@ pnpm install
 pnpm run build
 cp -r dist/* ../src/main/resources/dist
 wget https://github.com/wushuo894/ani-rss-update/releases/download/latest/ani-rss-update.exe
-cp ani-rss-update.exe ../src/main/resources/ani-rss-update.exe
+mv ani-rss-update.exe ../src/main/resources/ani-rss-update.exe
 cd ..
 mvn -B package -DskipTests --file pom.xml
