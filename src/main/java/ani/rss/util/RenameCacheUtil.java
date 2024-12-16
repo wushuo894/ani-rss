18c9b955ec9ed2c9ae393dd1ad1831b818db50b6
wushuo <wushuo894@outlook.com>
2024-12-16 09:43:53 +0800

    优化了aria2与tr的重命名

---
+++ src/main/java/ani/rss/util/RenameCacheUtil.java
@@ -0,0 +1,99 @@
+package ani.rss.util;
+
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.text.StrFormatter;
+import cn.hutool.db.DbUtil;
+import cn.hutool.db.Entity;
+import cn.hutool.db.SqlConnRunner;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 使用ehcache持久化存储
+ */
+@Slf4j
+public class RenameCacheUtil {
+    private static final String TABLE_NAME = "RENAME_CACHES";
+    private static Connection connection;
+    private static SqlConnRunner sqlConnRunner;
+
+    private static synchronized void connection() {
+        if (Objects.nonNull(connection)) {
+            return;
+        }
+
+        File configDir = ConfigUtil.getConfigDir();
+        String absolutePath = FileUtil.getAbsolutePath(configDir.toString());
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(StrFormatter.format("jdbc:sqlite:{}/database.db", absolutePath));
+
+            Statement statement = connection.createStatement();
+
+            String sql = StrFormatter.format("CREATE TABLE IF NOT EXISTS {} (K INTEGER PRIMARY KEY, V TEXT)", TABLE_NAME);
+            statement.execute(sql);
+
+            sqlConnRunner = DbUtil.newSqlConnRunner(connection);
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> DbUtil.close(connection)));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static synchronized void put(String key, String object) {
+        remove(key);
+        log.debug("put => key: {}, object: {}", key, object);
+        try {
+            sqlConnRunner.insert(connection,
+                    new Entity(TABLE_NAME)
+                            .set("K", key)
+                            .set("V", object)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static synchronized String get(String key) {
+        connection();
+        log.debug("get => key: {}", key);
+        try {
+            List<Entity> list = sqlConnRunner.find(connection,
+                    new Entity(TABLE_NAME)
+                            .set("K", key)
+            );
+            if (list.isEmpty()) {
+                return null;
+            }
+            return (String) list.get(0).get("V");
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static synchronized void remove(String key) {
+        connection();
+        log.debug("remove => key: {}", key);
+        try {
+            sqlConnRunner.del(connection,
+                    new Entity(TABLE_NAME)
+                            .set("K", key)
+            );
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+}
