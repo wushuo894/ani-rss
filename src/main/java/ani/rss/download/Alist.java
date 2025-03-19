d4811d9a894ce0fe4a87b0f9f337facfedbb18ea
wushuo <wushuo894@outlook.com>
2025-03-20 04:48:50 +0800

    支持使用alist离线下载到115、PikPak

--- src/main/java/ani/rss/download/Alist.java
+++ src/main/java/ani/rss/download/Alist.java
@@ -3,6 +3,7 @@ package ani.rss.download;
 import ani.rss.entity.Config;
 import ani.rss.entity.Item;
 import ani.rss.entity.TorrentsInfo;
+import ani.rss.enums.StringEnum;
 import ani.rss.util.ExceptionUtil;
 import ani.rss.util.GsonStatic;
 import ani.rss.util.HttpReq;
@@ -11,8 +12,10 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.Assert;
 import cn.hutool.core.text.StrFormatter;
 import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.ReUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.Header;
+import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -22,10 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 public class Alist implements BaseDownload {
@@ -43,8 +43,7 @@ public class Alist implements BaseDownload {
         String downloadPath = config.getDownloadPath();
         Assert.notBlank(downloadPath, "未设置下载位置");
         try {
-            return HttpReq.post(host + "/api/fs/list", false)
-                    .header(Header.AUTHORIZATION, password)
+            return fsApi("list")
                     .body(GsonStatic.toJson(Map.of(
                             "path", downloadPath,
                             "page", 1,
@@ -89,14 +88,29 @@ public class Alist implements BaseDownload {
 
     @Override
     public Boolean download(Item item, String savePath, File torrentFile, Boolean ova) {
-        String host = config.getHost();
-        String password = config.getPassword();
         String magnet = TorrentUtil.getMagnet(torrentFile);
         String reName = item.getReName();
         String path = savePath + "/" + reName;
+        Boolean backRss = config.getBackRss();
+        Boolean delete = config.getDelete();
         try {
-            HttpReq.post(host + "/api/fs/add_offline_download", false)
-                    .header(Header.AUTHORIZATION, password)
+            // 洗版，删除备 用RSS 所下载的视频
+            if (backRss && delete) {
+                String s = ReUtil.get(StringEnum.SEASON_REG, reName, 0);
+                ls(savePath)
+                        .stream()
+                        .map(AlistFileInfo::getName)
+                        .filter(name -> name.contains(s))
+                        .forEach(name -> {
+                            fsApi("remove")
+                                    .body(GsonStatic.toJson(Map.of(
+                                            "dir", savePath,
+                                            "names", List.of(name)
+                                    ))).then(HttpResponse::isOk);
+                            log.info("已开启备用RSS, 自动删除 {}/{}", savePath, name);
+                        });
+            }
+            fsApi("add_offline_download")
                     .body(GsonStatic.toJson(Map.of(
                             "path", path,
                             "urls", List.of(magnet),
@@ -108,13 +122,8 @@ public class Alist implements BaseDownload {
                         Assert.isTrue(jsonObject.get("code").getAsInt() == 200, "添加离线下载失败 {}", reName);
                     });
             log.info("添加离线下载成功");
-            for (int i = 0; i < 60; i++) {
-                Thread.sleep(3000);
-                if (ls(savePath)
-                        .stream()
-                        .noneMatch(alistFileInfo -> alistFileInfo.getName().equals(reName))) {
-                    continue;
-                }
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(5000);
                 List<AlistFileInfo> alistFileInfos = ls(path);
                 if (alistFileInfos.isEmpty()) {
                     continue;
@@ -134,20 +143,17 @@ public class Alist implements BaseDownload {
                                     "new_name", reName + "." + FileUtil.extName(alistFileInfo.getName())
                             );
                         }).toList();
-                HttpReq.post(host + "/api/fs/batch_rename", false)
-                        .header(Header.AUTHORIZATION, password)
+                fsApi("batch_rename")
                         .body(GsonStatic.toJson(Map.of(
                                 "src_dir", path + "/" + parentName,
                                 "rename_objects", rename_objects
                         ))).then(res -> log.info(res.body()));
 
                 // 移动
-                Thread.sleep(2000);
                 List<String> names = alistFileInfos.stream()
                         .map(alistFileInfo -> reName + "." + FileUtil.extName(alistFileInfo.getName()))
                         .toList();
-                HttpReq.post(host + "/api/fs/recursive_move", false)
-                        .header(Header.AUTHORIZATION, password)
+                fsApi("recursive_move")
                         .body(GsonStatic.toJson(Map.of(
                                 "src_dir", path + "/" + parentName,
                                 "dst_dir", savePath,
@@ -155,9 +161,7 @@ public class Alist implements BaseDownload {
                         ))).then(res -> log.info(res.body()));
 
                 // 删除残留文件夹
-                Thread.sleep(2000);
-                HttpReq.post(host + "/api/fs/remove", false)
-                        .header(Header.AUTHORIZATION, password)
+                fsApi("remove")
                         .body(GsonStatic.toJson(Map.of(
                                 "dir", savePath,
                                 "names", List.of(reName)
@@ -196,23 +200,27 @@ public class Alist implements BaseDownload {
     }
 
     public List<AlistFileInfo> ls(String path) {
-        ThreadUtil.sleep(2000);
-        String host = config.getHost();
-        String password = config.getPassword();
-        return HttpReq.post(host + "/api/fs/list", false)
-                .header(Header.AUTHORIZATION, password)
-                .body(GsonStatic.toJson(Map.of(
-                        "path", path,
-                        "page", 1,
-                        "per_page", 0,
-                        "refresh", false
-                )))
-                .thenFunction(res -> {
-                    JsonObject jsonObject = GsonStatic.fromJson(res.body(), JsonObject.class);
-                    JsonArray jsonArray = jsonObject.getAsJsonObject("data")
-                            .getAsJsonArray("content");
-                    return GsonStatic.fromJsonList(jsonArray, AlistFileInfo.class);
-                });
+        try {
+            return fsApi("list")
+                    .body(GsonStatic.toJson(Map.of(
+                            "path", path,
+                            "page", 1,
+                            "per_page", 0,
+                            "refresh", false
+                    )))
+                    .thenFunction(res -> {
+                        JsonObject jsonObject = GsonStatic.fromJson(res.body(), JsonObject.class);
+                        JsonObject data = jsonObject.getAsJsonObject("data");
+                        if (Objects.isNull(data)) {
+                            return List.of();
+                        }
+                        JsonArray jsonArray = data.getAsJsonArray("content");
+                        return GsonStatic.fromJsonList(jsonArray, AlistFileInfo.class);
+                    });
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return List.of();
     }
 
     @Data
@@ -223,6 +231,20 @@ public class Alist implements BaseDownload {
         private Boolean is_dir;
         private Date modified;
         private Date created;
+    }
+
+    /**
+     * fs api
+     *
+     * @param action
+     * @return
+     */
+    public synchronized HttpRequest fsApi(String action) {
+        ThreadUtil.sleep(1000);
+        String host = config.getHost();
+        String password = config.getPassword();
+        return HttpReq.post(host + "/api/fs/" + action, false)
+                .header(Header.AUTHORIZATION, password);
     }
 
 }
