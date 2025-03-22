88ed6417ba75a8c4da6cc87a143000b17a443e4b
wushuo <wushuo894@outlook.com>
2025-03-22 21:15:12 +0800

    观看视频支持调用外部播放器

--- ui/src/play/Artplayer.vue
+++ ui/src/play/Artplayer.vue
@@ -1,17 +1,46 @@
 <template>
   <div>
     <div class="artplayer-app"></div>
+    <div class="flex" style="margin-top: 8px;width: 100%;justify-content: end;">
+      <el-button bg text @click="openUrl(`potplayer://${src}`)">
+        <template #icon>
+          <img alt="PotPlayer" class="el-icon--left" height="20" src="../icon/icon-PotPlayer.webp" width="20"/>
+        </template>
+        Pot
+      </el-button>
+      <el-button bg text @click="openUrl(`vlc://${src}`)">
+        <template #icon>
+          <img alt="VLC" class="el-icon--left" height="20" src="../icon/icon-VLC.webp" width="20"/>
+        </template>
+        VLC
+      </el-button>
+      <el-button bg text @click="openUrl(`iina://weblink?url=${src}`)">
+        <template #icon>
+          <img alt="IINA" class="el-icon--left" height="20" src="../icon/icon-IINA.webp" width="20"/>
+        </template>
+        IINA
+      </el-button>
+    </div>
   </div>
 </template>
 
 <script setup>
-import {onMounted} from 'vue'
+import {onMounted, ref} from 'vue'
 import Artplayer from 'artplayer';
 import artplayerPluginMultipleSubtitles from 'artplayer-plugin-multiple-subtitles';
 
 const props = defineProps(['src', 'subtitles'])
 
+let openUrl = (url) => {
+  console.log('openUrl', url)
+  window.open(url)
+}
+
+let src = ref('')
+
 onMounted(() => {
+  src.value = encodeURI(location.href + props.src)
+
   let selector = props.subtitles
   let defaultName = ''
   let settings = []
