c7cb0ce9bc6193a3289de7207cec36e32acf9758
wushuo <wushuo894@outlook.com>
2025-03-23 20:17:57 +0800

    优化跳过下载时的日志

--- src/main/java/ani/rss/util/UpdateUtil.java
+++ src/main/java/ani/rss/util/UpdateUtil.java
@@ -12,7 +12,6 @@ import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.RuntimeUtil;
 import cn.hutool.core.util.StrUtil;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 
@@ -32,15 +31,11 @@ public class UpdateUtil {
                 .setLatest("")
                 .setBody("");
         try {
-            HttpReq.get("https://api.github.com/repos/wushuo894/ani-rss/releases?page=1&per_page=1", true)
+            HttpReq.get("https://api.github.com/repos/wushuo894/ani-rss/releases/latest", true)
                     .timeout(3000)
                     .then(response -> {
                         Assert.isTrue(response.isOk(), "status: {}", response.getStatus());
-                        JsonArray jsonArray = GsonStatic.fromJson(response.body(), JsonArray.class);
-                        if (jsonArray.isEmpty()) {
-                            return;
-                        }
-                        JsonObject jsonObject = jsonArray.get(0).getAsJsonObject();
+                        JsonObject jsonObject = GsonStatic.fromJson(response.body(), JsonObject.class);
                         String latest = jsonObject.get("name").getAsString().replace("v", "");
                         about.setUpdate(VersionComparator.INSTANCE.compare(latest, version) > 0)
                                 .setLatest(latest);
