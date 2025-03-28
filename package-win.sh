23a7969a45294d482a7383371b184f9f75cf9fe1
wushuo <wushuo894@outlook.com>
2025-03-28 20:05:18 +0800

    1.1.337

--- package-win.sh
+++ package-win.sh
@@ -7,10 +7,10 @@ cp ani-rss-launcher.exe ani-rss
 
 sudo apt update
 sudo apt install zip unzip
-wget https://github.com/ibmruntimes/semeru17-binaries/releases/download/jdk-17.0.14%2B7_openj9-0.49.0/ibm-semeru-open-jre_x64_windows_17.0.14_7_openj9-0.49.0.zip
-unzip ibm-semeru-open-jre_x64_windows_17.0.14_7_openj9-0.49.0.zip
-mv jdk-17.0.14+7-jre ani-rss/jre
+wget https://github.com/ojdkbuild/ojdkbuild/releases/download/java-17-openjdk-17.0.3.0.6-1/java-17-openjdk-17.0.3.0.6-1.jre.win.x86_64.zip
+unzip java-17-openjdk-17.0.3.0.6-1.jre.win.x86_64.zip
+mv java-17-openjdk-17.0.3.0.6-1.jre.win.x86_64 ani-rss/jre
 cp ../windows/* ani-rss
-zip -r ani-rss.win.x64.zip ani-rss
+zip -r ani-rss.win.x86_64.zip ani-rss
 
-md5sum ani-rss.win.x64.zip | awk '{print $1}' > ani-rss.win.x64.zip.md5
+md5sum ani-rss.win.x86_64.zip | awk '{print $1}' > ani-rss.win.x86_64.zip.md5
