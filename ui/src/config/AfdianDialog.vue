184a68a74b3a0100eee71b99c09e7447bfc8e454
wushuo <wushuo894@outlook.com>
2025-04-06 22:09:39 +0800

    优化设置页面布局

---
+++ ui/src/config/AfdianDialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-dialog v-model="dialogVisible" align-center center style="max-width: 600px;" title="爱发电">
+    <Afdian v-if="dialogVisible" :config="props.config"/>
+  </el-dialog>
+</template>
+
+
+<script setup>
+
+import {ref} from "vue";
+import Afdian from "../config/Afdian.vue";
+
+let dialogVisible = ref(false)
+
+let show = () => {
+  dialogVisible.value = true
+}
+
+defineExpose({show})
+let props = defineProps(['config'])
+</script>
