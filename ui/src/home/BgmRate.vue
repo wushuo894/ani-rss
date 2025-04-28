e33c9d374f6a51a0c22e9a0257f10a19ba51060a
wushuo <wushuo894@outlook.com>
2025-04-28 23:21:05 +0800

    1.2.38

--- ui/src/home/BgmRate.vue
+++ ui/src/home/BgmRate.vue
@@ -2,7 +2,12 @@
   <el-dialog v-model="dialogVisible" align-center center style="max-width: 300px;" title="评分">
     <div v-loading="loading">
       <div class="flex" style="justify-content: center;width: 100%;">
-        <el-rate v-model="ani.score" :max="10" :min="0"/>
+        <el-rate v-model="ani.score"
+                 :max="10"
+                 :min="0"/>
+      </div>
+      <div style="height: 12px;">
+        <p v-if="ani.score > 0" style="text-align: center;">{{ texts[ani.score - 1] }}</p>
       </div>
       <div class="flex" style="width: 100%;justify-content: space-between;margin-top: 14px;">
         <el-button :icon="Ban" bg text @click="clearRate">清空评分</el-button>
@@ -19,6 +24,11 @@ import {ref} from "vue";
 import api from "../api.js";
 import {Ban, Save} from "@vicons/fa";
 import {ElMessage} from "element-plus";
+
+let texts = ref([
+    '不忍直视 1 (请谨慎评价)', '很差 2', '差 3', '较差 4', '不过不失 5',
+    '还行 6','推荐 7','力荐 8','神作 9','超神作 10 (请谨慎评价)'
+])
 
 let dialogVisible = ref(false)
 
