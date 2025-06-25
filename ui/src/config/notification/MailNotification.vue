1fca75ca168759f705d84692dd4b8414e99942ec
wushuo <wushuo894@outlook.com>
2025-06-25 17:00:43 +0800

    优化通知模板

--- ui/src/config/notification/MailNotification.vue
+++ ui/src/config/notification/MailNotification.vue
@@ -1,5 +1,9 @@
 <template>
   <template v-if="props.notificationConfig['notificationType'] === 'MAIL'">
+    <el-form-item label="通知模版">
+      <el-input v-model:model-value="props.notificationConfig['notificationTemplate']" :autosize="{ minRows: 2}"
+                placeholder="${notification}" type="textarea"/>
+    </el-form-item>
     <el-form-item label="SMTP地址">
       <el-input v-model:model-value="props.notificationConfig['mailSMTPHost']" placeholder="smtp.qq.com"/>
     </el-form-item>
