df37de5368979bbc623aeb12326b5c2ce6d9c0b0
sosorin <linsu817@gmail.com>
2025-07-28 17:49:41 +0800

    Base64 Image Support for WebHook Notification

--- src/main/java/ani/rss/notification/WebHookNotification.java
+++ src/main/java/ani/rss/notification/WebHookNotification.java
@@ -3,13 +3,18 @@ package ani.rss.notification;
 import ani.rss.entity.Ani;
 import ani.rss.entity.NotificationConfig;
 import ani.rss.enums.NotificationStatusEnum;
+import ani.rss.util.ConfigUtil;
 import ani.rss.util.HttpReq;
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.Opt;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.Method;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.Base64;
 
 /**
  * WebHook
@@ -49,6 +54,13 @@ public class WebHookNotification implements BaseNotification {
         webHookUrl = webHookUrl.replace("${image}", image);
         webHookBody = webHookBody.replace("${image}", image);
 
+        if (StrUtil.contains(webHookBody, "${imageBase64}")) {
+            String imageBase64 = getImageBase64FromFile(ani);
+
+            webHookUrl = webHookUrl.replace("${imageBase64}", imageBase64);
+            webHookBody = webHookBody.replace("${imageBase64}", imageBase64);
+        }
+
         HttpRequest httpRequest = HttpReq.get(webHookUrl, true)
                 .method(Method.valueOf(webHookMethod));
 
@@ -56,5 +68,26 @@ public class WebHookNotification implements BaseNotification {
             httpRequest.body(webHookBody);
         }
         return httpRequest.thenFunction(HttpResponse::isOk);
+    }
+
+    private static String getImageBase64FromFile(Ani ani) {
+        // 1*1 pixel gif
+        String imageBase64 = "R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+
+        imageBase64 = Opt.ofNullable(ani)
+                .map(a -> {
+                    String filename = a.getCover();
+                    File file = new File(filename);
+                    File configDir = ConfigUtil.getConfigDir();
+                    if (!file.exists()) {
+                        file = new File(configDir + "/files/" + filename);
+                    }
+                    if (!file.exists()) {
+                        return null;
+                    }
+                    return Base64.getEncoder().encodeToString(FileUtil.readBytes(file));
+                }).filter(StrUtil::isNotBlank)
+                .orElse(imageBase64);
+        return imageBase64;
     }
 }
