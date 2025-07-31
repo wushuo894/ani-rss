37190d6d468aec1c4e8cb9f75b618dd73f4456e9
wushuo <wushuo894@outlook.com>
2025-07-31 13:15:43 +0800

    2.0.36

--- src/main/java/ani/rss/download/Aria2.java
+++ src/main/java/ani/rss/download/Aria2.java
@@ -55,6 +55,7 @@ public class Aria2 implements BaseDownload {
         ThreadUtil.sleep(1000);
         try {
             torrentsInfos.addAll(getTorrentsInfos("aria2/tellActive.json"));
+            torrentsInfos.addAll(getTorrentsInfos("aria2/tellWaiting.json"));
             torrentsInfos.addAll(getTorrentsInfos("aria2/tellStopped.json"));
         } catch (Exception e) {
             log.error(e.getMessage(), e);
@@ -227,7 +228,7 @@ public class Aria2 implements BaseDownload {
                 .sorted(Comparator.comparingLong(file -> Long.MAX_VALUE - file.length()))
                 .toList();
 
-        Assert.notEmpty(files);
+        Assert.notEmpty(files, "映射路径存在错误, 无法重命名");
 
         for (File src : files) {
             String name = src.getName();
