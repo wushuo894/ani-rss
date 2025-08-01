09d746200f4220aaea6d6fc9bac59c2b32bf673c
wushuo <wushuo894@outlook.com>
2025-08-02 00:02:00 +0800

    修复element-plus中文化

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,15 +1,11 @@
 import {createApp} from 'vue'
-import Login from './Login.vue'
+import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import {zhCn} from "element-plus/es/locale/index";
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './style.css'
 
-const app = createApp(Login)
-app.provide('$ELEMENT', {
-    locale: zhCn,
-})
+const app = createApp(App)
 // 引入图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
