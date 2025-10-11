93e9e29de46423871bd1fdf806abf1964665560c
wushuo <wushuo894@outlook.com>
2025-10-11 22:47:52 +0800

    2.2.15

--- docker/run.sh
+++ docker/run.sh
@@ -43,6 +43,7 @@ do
       -XX:-ShrinkHeapInSteps \
       -XX:TieredStopAtLevel=1 \
       -XX:+IgnoreUnrecognizedVMOptions \
+      --enable-native-access=ALL-UNNAMED \
       --add-opens=java.base/java.net=ALL-UNNAMED \
       --add-opens=java.base/sun.net.www.protocol.https=ALL-UNNAMED \
       -jar $jar_path --port $port &
