a5053b453490a5520759e8cb554e85f0f5cc0e9b
wushuo <wushuo894@outlook.com>
2025-11-09 01:29:07 +0800

    chore: 补全注释

--- ui/src/Main.vue
+++ ui/src/Main.vue
@@ -13,26 +13,36 @@ import {useDark} from "@vueuse/core";
 import {authorization, color, colorChange, isNotMobile, maxContentWidth} from "@/js/global.js";
 import {reactive} from "vue";
 
+/**
+ * 链接配置
+ */
 let linkConfig = reactive({
   type: 'primary',
   underline: 'never'
 })
 
+/**
+ * 夜间模式
+ */
 useDark({
   onChanged: dark => {
+    // 自动根据夜间模式修改沉浸式状态栏
     const meta = document.getElementById('themeColorMeta');
     meta.content = dark ? '#000000' : '#ffffff';
   }
 })
 
+// 修改强调色
 colorChange(color.value)
 
 let app = document.querySelector('#app');
 
+// 设置最大布局宽度
 maxContentWidth.value = Math.max(maxContentWidth.value, 1200)
 app
     .style.maxWidth = `${maxContentWidth.value}px`
 
+// 是否非移动设备
 isNotMobile.value = app.offsetWidth > 800
 
 </script>
