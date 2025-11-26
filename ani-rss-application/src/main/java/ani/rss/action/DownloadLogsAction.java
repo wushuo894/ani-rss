c05c4feaec09d3ccb80dfe2c9a56c7a6380fa1ac
wushuo <wushuo894@outlook.com>
2025-11-26 20:33:34 +0800

    feat: 优化日志下载

--- ani-rss-application/src/main/java/ani/rss/action/DownloadLogsAction.java
+++ ani-rss-application/src/main/java/ani/rss/action/DownloadLogsAction.java
@@ -3,13 +3,15 @@ package ani.rss.action;
 import ani.rss.annotation.Auth;
 import ani.rss.annotation.Path;
 import ani.rss.util.other.ConfigUtil;
-import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.text.StrFormatter;
 import cn.hutool.core.util.ZipUtil;
 import cn.hutool.http.server.HttpServerRequest;
 import cn.hutool.http.server.HttpServerResponse;
+import lombok.Cleanup;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -22,8 +24,17 @@ public class DownloadLogsAction implements BaseAction {
     public void doAction(HttpServerRequest request, HttpServerResponse response) throws IOException {
         File configDir = ConfigUtil.getConfigDir();
         String logsPath = configDir + "/logs";
-        File tempFile = FileUtil.createTempFile();
-        ZipUtil.zip(tempFile, StandardCharsets.UTF_8, false, new File(logsPath));
-        response.write(tempFile, "logs.zip");
+
+        String filename = "logs.zip";
+
+        String contentType = getContentType(filename);
+
+        response.setContentType(contentType);
+        response.setHeader("Content-Disposition", StrFormatter.format("inline; filename=\"{}\"", filename));
+
+        @Cleanup
+        OutputStream outputStream = response.getOut();
+
+        ZipUtil.zip(outputStream, StandardCharsets.UTF_8, false, s -> true, new File(logsPath));
     }
 }
