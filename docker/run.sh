775859a94a5bad69d303741d6e9c3b47e65bbca6
wushuo <wushuo894@outlook.com>
2025-11-26 09:41:40 +0800

    feat: 增加紧凑对象头参数

--- docker/run.sh
+++ docker/run.sh
@@ -44,6 +44,7 @@ do
       -XX:-ShrinkHeapInSteps \
       -XX:TieredStopAtLevel=1 \
       -XX:+IgnoreUnrecognizedVMOptions \
+      -XX:+UseCompactObjectHeaders \
       --enable-native-access=ALL-UNNAMED \
       --add-opens=java.base/java.net=ALL-UNNAMED \
       --add-opens=java.base/sun.net.www.protocol.https=ALL-UNNAMED \
