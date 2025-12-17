f76c2180d01811bbd5ba0f9aeb826f1bb9c7db25
wushuo <wushuo894@outlook.com>
2025-12-17 19:04:12 +0800

    fix: 修复windows打包

--- ani-rss-application/generate-resources.sh
+++ ani-rss-application/generate-resources.sh
@@ -19,8 +19,12 @@ git branch --show-current >> ${build_info_path}
 update_exe_path=./src/main/resources/ani-rss-update.exe
 
 if [ ! -e ${update_exe_path} ]; then
-  echo "下载 ani-rss-update.exe"
-  curl -o ani-rss-update.exe https://github.com/wushuo894/ani-rss-update/releases/download/latest/ani-rss-update.exe
+  echo "${YELLOW}下载 ani-rss-update.exe${NC}"
+  curl -L https://github.com/wushuo894/ani-rss-update/releases/download/latest/ani-rss-update.exe -O ani-rss-update.exe
+  if [ $? -eq 1 ]; then
+    echo -e "${RED}下载失败 ani-rss-update.exe${NC}"
+    exit 1
+  fi
   mv ani-rss-update.exe ${update_exe_path}
 else
   echo -e "${YELLOW}已存在 ani-rss-update.exe${NC}"
