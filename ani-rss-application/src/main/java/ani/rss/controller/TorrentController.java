a2a2637a9895a59702383a577b78703a45eb5d9c
lanfunoe <lanfunoe@qq.com>
2026-02-24 19:11:52 +0800

    feat(torrent): add torrent cache deletion endpoint

---
+++ ani-rss-application/src/main/java/ani/rss/controller/TorrentController.java
@@ -0,0 +1,49 @@
+package ani.rss.controller;
+
+import ani.rss.annotation.Auth;
+import ani.rss.commons.FileUtils;
+import ani.rss.entity.Ani;
+import ani.rss.entity.Result;
+import ani.rss.util.other.AniUtil;
+import ani.rss.util.other.TorrentUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RestController
+public class TorrentController {
+
+    @Auth
+    @Operation(summary = "删除缓存种子")
+    @DeleteMapping("/torrent")
+    public Result<Void> del(@RequestParam("id") String id, @RequestParam("infoHash") String infoHash) {
+        Optional<Ani> first = AniUtil.ANI_LIST.stream()
+                .filter(ani -> id.equals(ani.getId()))
+                .findFirst();
+        if (first.isEmpty()) {
+            return Result.error("此订阅不存在");
+        }
+
+        List<String> infoHashList = StrUtil.split(infoHash, ",", true, true);
+        Ani ani = first.get();
+        File torrentDir = TorrentUtil.getTorrentDir(ani);
+        File[] files = FileUtils.listFiles(torrentDir);
+        for (File file : files) {
+            String name = FileUtil.mainName(file);
+            if (infoHashList.contains(name)) {
+                log.info("删除种子 {}", file);
+                FileUtil.del(file);
+            }
+        }
+        return Result.success("删除完成");
+    }
+}
