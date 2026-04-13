8dfe8770b1947111ea71f773287f4b11976841cb
wushuo <wushuo894@outlook.com>
2026-04-14 07:25:28 +0800

    refactor: 优化系统托盘

--- ani-rss-application/src/main/java/ani/rss/AniRssApplication.java
+++ ani-rss-application/src/main/java/ani/rss/AniRssApplication.java
@@ -1,7 +1,7 @@
 package ani.rss;
 
 import ani.rss.entity.Global;
-import ani.rss.util.other.MenuUtil;
+import ani.rss.util.other.SystemTrayUtil;
 import cn.hutool.core.util.ObjectUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,11 +17,15 @@ public class AniRssApplication {
     public static void main(String[] args) {
         Global.ARGS = List.of(ObjectUtil.defaultIfNull(args, new String[]{}));
         loadProperty();
-        MenuUtil.start();
+        SystemTrayUtil.start();
         SpringApplication.run(AniRssApplication.class, args);
     }
 
     public static void loadProperty() {
+        // 隐藏dock栏图标
+        System.setProperty("apple.awt.UIElement", "true");
+        // 菜单栏图标颜色自适应
+        System.setProperty("apple.awt.enableTemplateImages", "true");
         // 启用Basic认证
         System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
         // DNS解析成功过期时间
---
+++ ani-rss-application/src/main/java/ani/rss/util/other/SystemTrayUtil.java
@@ -0,0 +1,74 @@
+package ani.rss.util.other;
+
+import ani.rss.entity.Global;
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.extra.spring.SpringUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.net.URL;
+
+@Slf4j
+public class SystemTrayUtil {
+
+    public static TrayIcon TRAY_ICON;
+
+    public static void start() {
+        // 仅在添加--gui参数时启动托盘
+        if (Global.ARGS.contains("--gui")) {
+            try {
+                showSystemTray();
+                log.info("启动系统托盘已启动");
+            } catch (Exception e) {
+                log.error("启动系统托盘失败", e);
+                System.exit(1);
+            }
+        }
+    }
+
+    /**
+     * 显示系统托盘
+     * 参考 <a href="https://lanlan2017.github.io/blog/2b294c47/">链接</a>
+     */
+    private static void showSystemTray() throws AWTException {
+        if (!SystemTray.isSupported()) {
+            log.error("系统托盘不支持, 将以命令行方式启动");
+            // 直接输出到控制台，不使用logger
+            System.out.print("系统托盘不支持, 将以命令行方式启动");
+            return;
+        }
+
+        // 创建JPopupMenu
+        // 重写firePopupMenuWillBecomeInvisible
+        // 消失后将绑定的组件一起消失
+        PopupMenu popupMenu = new PopupMenu();
+
+        // 添加菜单选项
+        MenuItem webui = popupMenu.add(new MenuItem("WebUI"));
+        webui.addActionListener(e -> {
+            if (Desktop.isDesktopSupported()) {
+                try {
+                    String port = SpringUtil.getProperty("server.port");
+                    Desktop.getDesktop().browse(new URL("http://127.0.0.1:" + port).toURI());
+                } catch (Exception ex) {
+                    log.error("打开webui失败", ex);
+                }
+            }
+        });
+        MenuItem exit = popupMenu.add(new MenuItem("Quit", new MenuShortcut(KeyEvent.VK_Q)));
+        exit.addActionListener(e -> {
+            log.info("使用系统托盘退出");
+            System.exit(0);
+        });
+
+        SystemTray tray = SystemTray.getSystemTray();
+        TRAY_ICON = new TrayIcon(
+                Toolkit.getDefaultToolkit().getImage(
+                        ResourceUtil.getResource("image/icon-64.png")
+                ), "ani-rss", popupMenu);
+        TRAY_ICON.setImageAutoSize(true);
+
+        tray.add(TRAY_ICON);
+    }
+}
