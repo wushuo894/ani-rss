413084a2030e89a455a88d6782d576751813e23a
wushuo <wushuo894@outlook.com>
2026-04-20 09:32:38 +0800

    build: 优化构建

--- ani-rss-application/generate-resources.sh
+++ ani-rss-application/generate-resources.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-# build_info 位置
-build_info_path=./src/main/resources/build_info
+# 定义颜色代码
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 
-rm -rf ${build_info_path}
-git rev-parse --short HEAD >> ${build_info_path}
-git branch --show-current >> ${build_info_path}
+cd $(dirname $0)/src/main/resources
 
-# 更新程序位置
-update_exe_path=./src/main/resources/ani-rss-update.exe
+# build.info
+BUILD_INFO_FILE="build.info"
 
-if [ ! -e ${update_exe_path} ]; then
-  echo "${YELLOW}下载 ani-rss-update.exe${NC}"
-  curl -L https://github.com/wushuo894/ani-rss-update/releases/download/latest/ani-rss-update.exe -O ani-rss-update.exe
+rm -rf ${BUILD_INFO_FILE}
+git rev-parse --short HEAD >> ${BUILD_INFO_FILE}
+git branch --show-current >> ${BUILD_INFO_FILE}
+
+echo "$(cat ${BUILD_INFO_FILE})"
+
+# 更新程序
+UPDATE_EXE_FILE="ani-rss-update.exe"
+
+if [ ! -e ${UPDATE_EXE_FILE} ]; then
+  echo -e "${YELLOW}下载 ${UPDATE_EXE_FILE}${NC}"
+  curl -L https://github.com/wushuo894/ani-rss-update/releases/download/latest/ani-rss-update.exe -o ${UPDATE_EXE_FILE}
   if [ $? -eq 1 ]; then
-    echo -e "${RED}下载失败 ani-rss-update.exe${NC}"
+    echo -e "${RED}下载失败 ${UPDATE_EXE_FILE}${NC}"
     exit 1
   fi
-  mv ani-rss-update.exe ${update_exe_path}
+  echo -e "${GREEN}下载完成 ${UPDATE_EXE_FILE}${NC}"
 else
-  echo -e "${YELLOW}已存在 ani-rss-update.exe${NC}"
+  echo -e "${YELLOW}已存在 ${UPDATE_EXE_FILE}${NC}"
 fi
