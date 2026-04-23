cc69f799d99354abc054600a7c6efee1a557bca6
wushuo <wushuo894@outlook.com>
2026-04-23 09:34:24 +0800

    feat: AnimeGarden

---
+++ ani-rss-application/src/main/java/ani/rss/controller/AnimeGardenController.java
@@ -0,0 +1,32 @@
+package ani.rss.controller;
+
+import ani.rss.annotation.Auth;
+import ani.rss.entity.AnimeGarden;
+import ani.rss.entity.web.Result;
+import ani.rss.util.other.AnimeGardenUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class AnimeGardenController {
+
+    @Auth
+    @Operation(summary = "AnimeGarden 番剧列表")
+    @PostMapping("/animeGardenList")
+    public Result<List<AnimeGarden.Week>> animeGardenList() {
+        return Result.success(AnimeGardenUtil.list());
+    }
+
+    @Auth
+    @Operation(summary = "AnimeGarden 番剧字幕组列表")
+    @PostMapping("/animeGardenGroup")
+    public Result<List<AnimeGarden.Group>> animeGardenGroup(@RequestParam("bgmId") String bgmId) {
+        return Result.success(AnimeGardenUtil.group(bgmId));
+    }
+}
