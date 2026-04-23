64a03660ac21da781e24639594d8443febebaf73
wushuo <wushuo894@outlook.com>
2026-04-23 11:21:49 +0800

    fix: 修复AnimeGarden正则匹配

--- ani-rss-ui/src/home/AniBT.vue
+++ ani-rss-ui/src/home/AniBT.vue
@@ -62,7 +62,8 @@
                     <el-collapse-item v-for="anime in item.animes" :name="anime.bgmId">
                       <template #title>
                         <div class="flex collapse-title">
-                          <img :src="img(anime)" class="cover" @click.stop="open(anime.bgmId)">
+                          <img :src="img(anime)" class="cover"
+                               @click.stop="open('https://anibt.net/anime/'+anime.bgmId)">
                           <div class="flex collapse-title">
                             <el-text :truncated="false" line-clamp="1" size="small"
                                      class="title-text">
