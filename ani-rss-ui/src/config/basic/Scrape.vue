52d0029a587e67b5a374ec7498ecfef8411d43cc
wushuo <wushuo894@outlook.com>
2026-05-11 21:49:16 +0800

    feat: 增加刮削追更设置

---
+++ ani-rss-ui/src/config/basic/Scrape.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-form label-width="auto"
+           class="full-width">
+    <el-form-item label="自动刮削">
+      <el-switch v-model="props.config['scrape']"/>
+    </el-form-item>
+    <el-form-item label="追更天数">
+      <div>
+        <el-input-number v-model="props.config['followDay']" :min="1">
+          <template #suffix>
+            天
+          </template>
+        </el-input-number>
+        <br/>
+        <el-text class="mx-1" size="small">
+          自动强制刮削最近更新集的元数据
+        </el-text>
+      </div>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script setup>
+
+import {ElText} from "element-plus";
+
+let props = defineProps(['config'])
+</script>
